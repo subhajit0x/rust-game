00254f9bc5334f81047340745492fb2d1b4b1859
Hristiyan Genchev <genchev99@users.noreply.github.com>
2021-09-01 15:40:06 +0300

    Draw the map

--- src/assets.rs
+++ src/assets.rs
@@ -2,8 +2,10 @@ use ggez::audio;
 use ggez::audio::SoundSource;
 use ggez::graphics::{self, Color};
 use ggez::{Context, ContextBuilder, GameResult};
+use std::collections::HashMap;
 
 pub(crate) struct Assets {
+    tiles: HashMap<String, graphics::Image>,
     nexus_image: graphics::Image,
     heart_image: graphics::Image,
     enemy_image: graphics::Image,
@@ -17,6 +19,18 @@ pub(crate) struct Assets {
 
 impl Assets {
     pub(crate) fn new(ctx: &mut Context) -> GameResult<Assets> {
+        let mut tiles: HashMap<String, graphics::Image> = HashMap::new();
+        tiles.insert("6".to_string(), graphics::Image::new(ctx, "/06.png")?);
+        tiles.insert("14".to_string(), graphics::Image::new(ctx, "/14.png")?);
+        tiles.insert("19".to_string(), graphics::Image::new(ctx, "/19.png")?);
+        tiles.insert("33".to_string(), graphics::Image::new(ctx, "/33.png")?);
+        tiles.insert("44".to_string(), graphics::Image::new(ctx, "/44.png")?);
+        tiles.insert("48".to_string(), graphics::Image::new(ctx, "/48.png")?);
+        tiles.insert("57".to_string(), graphics::Image::new(ctx, "/57.png")?);
+        tiles.insert("62".to_string(), graphics::Image::new(ctx, "/62.png")?);
+        tiles.insert("70".to_string(), graphics::Image::new(ctx, "/70.png")?);
+        tiles.insert("71".to_string(), graphics::Image::new(ctx, "/71.png")?);
+
         let nexus_image = graphics::Image::new(ctx, "/71.png")?;
         let heart_image = graphics::Image::new(ctx, "/71.png")?;
         let enemy_image = graphics::Image::new(ctx, "/71.png")?;
@@ -29,6 +43,7 @@ impl Assets {
         // let enemy_hit_sound = audio::Source::new(ctx, "/pew.ogg")?;
 
         Ok(Assets {
+            tiles,
             nexus_image,
             heart_image,
             enemy_image,
@@ -41,7 +56,13 @@ impl Assets {
         })
     }
 
-    pub(crate) fn get_tile_image(&mut self, tile_sprite: String) -> &mut graphics::Image{
-        return &mut self.nexus_image;
+    pub(crate) fn get_tile_image(&mut self, mut sprite_name: String) -> &graphics::Image {
+        println!("sprite_name: {}", sprite_name);
+        return if let Some(x) = self.tiles.get_mut(&sprite_name) {
+            x
+        } else {
+            &mut self.nexus_image
+            // panic!("The sprite {} was not found in the assets list", sprite_name);
+        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,20 @@ mod assets;
 
 use ggez::{event, graphics, Context, GameResult};
 use std::time::{Duration, Instant};
-use crate::config::{MILLIS_PER_UPDATE, SCREEN_SIZE};
+use crate::config::{MILLIS_PER_UPDATE, SCREEN_SIZE, GRID_SIZE};
 use crate::nexus::Nexus;
 use crate::enemy::Enemy;
 use crate::assets::Assets;
 use std::collections::VecDeque;
-use std::env;
+use std::{env, fs};
 use std::path;
 use ggez::graphics::Color;
+use serde_json;
 use crate::movement_helpers::GridPosition;
 
 struct GameState {
     assets: Assets,
+    map_json: serde_json::Value,
     nexus: Nexus,
     enemies: VecDeque<Enemy>,
     score: f32,
@@ -30,11 +32,14 @@ struct GameState {
 impl GameState {
     pub fn new(ctx: &mut Context) -> GameResult<Self> {
         let assets = Assets::new(ctx)?;
-        let (width, height) = graphics::drawable_size(ctx);
-        println!("width: {}, height: {}", width, height);
+        let map_json_file = fs::File::open(get_resource_path("map_layout.json"))
+            .expect("file should open read only");
+        let map_json: serde_json::Value = serde_json::from_reader(map_json_file)
+            .expect("file should be proper JSON");
 
         Ok(GameState {
             assets,
+            map_json,
             nexus: Nexus::new((16, 19).into(), 3),
             enemies: VecDeque::new(),
             score: 0.0,
@@ -54,15 +59,38 @@ impl GameState {
     }
 }
 
-fn draw_map(assets: &mut Assets, ctx: &mut Context) -> GameResult {
-    let image = assets.get_tile_image("".to_string());
-    let dest: ggez::mint::Point2<f32> = GridPosition::new(63, 31).into();
-    // let offset: ggez::mint::Point2<f32> = GridPosition::new(4, 4).into();
+fn draw_map(assets: &mut Assets, ctx: &mut Context,  map_json: &mut serde_json::Value) {
+    for x in 0..GRID_SIZE.0 {
+        for y in 0..GRID_SIZE.1 {
+            let key = format!("{x}_{y}", x=x, y=y);
+            let image = assets.get_tile_image(map_json[key]["sprite"].to_string());
+            let dest: ggez::mint::Point2<f32> = GridPosition::new(x, y).into();
+            // let offset: ggez::mint::Point2<f32> = GridPosition::new(4, 4).into();
+            let drawparams = graphics::DrawParam::new()
+                .dest(dest);
+            // .offset(offset);
+            graphics::draw(ctx, image, drawparams);
+        }
+    }
+}
 
-    let drawparams = graphics::DrawParam::new()
-        .dest(dest);
-        // .offset(offset);
-    graphics::draw(ctx, image, drawparams)
+fn get_resources_dir() -> path::PathBuf {
+    let resources_dir = if let Ok(resources_dir) = env::var("RESOURCES_DIR") {
+        println!("manifest_dir: {}", resources_dir);
+        let mut path = path::PathBuf::from(resources_dir);
+        path
+    } else {
+        path::PathBuf::from("./resources")
+    };
+
+    resources_dir
+}
+
+fn get_resource_path(resource_name: &str) -> path::PathBuf {
+    let resources_dir = get_resources_dir();
+    let mut resources_dir_cloned = resources_dir.clone();
+    resources_dir_cloned.push(resource_name);
+    resources_dir_cloned
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
@@ -93,7 +121,8 @@ impl event::EventHandler<ggez::GameError> for GameState {
 
         {
             let assets = &mut self.assets;
-            draw_map(assets, ctx)?;
+            let map_json = &mut self.map_json;
+            draw_map(assets, ctx, map_json);
             for enemy in self.enemies.iter() {
                 enemy.draw(ctx)?;
             }
@@ -106,15 +135,7 @@ impl event::EventHandler<ggez::GameError> for GameState {
 }
 
 fn main() -> GameResult {
-    // We add the CARGO_MANIFEST_DIR/resources to the resource paths
-    // so that ggez will look in our cargo project directory for files.
-    let resource_dir = if let Ok(resources_dir) = env::var("RESOURCES_DIR") {
-        println!("manifest_dir: {}", resources_dir);
-        let mut path = path::PathBuf::from(resources_dir);
-        path
-    } else {
-        path::PathBuf::from("./resources")
-    };
+    let resources_dir = get_resources_dir();
 
     let (mut ctx, event_loop) = ggez::ContextBuilder::new("game", "Hristiyan Genchev")
         .window_setup(ggez::conf::WindowSetup::default().title("Game!"))
@@ -122,7 +143,7 @@ fn main() -> GameResult {
             ggez::conf::WindowMode::default()
                 .dimensions(SCREEN_SIZE.0 as f32, SCREEN_SIZE.1 as f32),
         )
-        .add_resource_path(resource_dir)
+        .add_resource_path(resources_dir)
         .build()
         .expect("Failed to build ggez context");
 
